8add63ce99d1e14744e863e3f2571dcaebb12128
Keith Boudreau <kboudy@gmail.com>
2019-12-09 10:52:18 -0500

    finished with 205

---
+++ src/context/AuthContext.js
@@ -0,0 +1,44 @@
+import { AsyncStorage } from 'react-native';
+import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
+import { navigate } from '../navigationRef';
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_error':
+      return { ...state, errorMessage: action.payload };
+    case 'signup':
+      return { ...state, token: action.payload, errorMessage: '' };
+    default:
+      return state;
+  }
+};
+
+const signup = dispatch => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post('/signup', { email, password });
+    const { token } = response.data;
+    await AsyncStorage.setItem('token', token);
+    dispatch({ type: 'signup', payload: token });
+    navigate('TrackList');
+  } catch (err) {
+    dispatch({
+      type: 'add_error',
+      payload: 'Something went wrong with signup'
+    });
+  }
+};
+
+const signin = dispatch => {
+  return ({ email, password }) => {};
+};
+
+const signout = dispatch => {
+  return () => {};
+};
+
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  { signin, signout, signup },
+  { token: null, errorMessage: '' }
+);
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  console.log(state);
 
   return (
     <View style={styles.container}>
@@ -29,8 +33,11 @@ const SignupScreen = ({ navigation }) => {
         value={password}
         onChangeText={setPassword}
       />
+      {state.errorMessage ? (
+        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
+      ) : null}
       <Spacer>
-        <Button title="Sign Up" />
+        <Button title="Sign Up" onPress={() => signup({ email, password })} />
       </Spacer>
     </View>
   );
@@ -47,6 +54,7 @@ const styles = StyleSheet.create({
     marginBottom: 200,
     flex: 1,
     justifyContent: 'center'
-  }
+  },
+  errorMessage: { fontSize: 16, color: 'red', marginLeft: 15, marginTop: 15 }
 });
 export default SignupScreen;
